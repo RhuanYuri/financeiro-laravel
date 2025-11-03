9125bfb5807a6bd1936917b8d56b6458565ff93b
RhuanYuri <rhuanyuri12@gmail.com>
2025-11-03 00:07:26 -0300

    chore: remove comment

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
-// 1. Importe o seu RevenueController
 use App\Http\Controllers\RevenueController;
 
 Route::get('/', function () {
@@ -17,23 +16,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-    // --- ADICIONE ESTAS LINHAS AQUI ---
-
-    // 2. Adicione a rota customizada 'getTotal'
-    // Ela deve vir *antes* do 'resource' para ter prioridade
     Route::get('/revenue/getTotal/{type}', [RevenueController::class, 'getTotal'])
         ->name('revenue.getTotal');
-
-    // 3. Adicione todas as rotas de "Recurso" (CRUD) para 'revenue'
-    // Isso cria automaticamente:
-    // GET /revenue          -> (método 'index' no controller)
-    // POST /revenue         -> (método 'store')
-    // PUT /revenue/{id}     -> (método 'update')
-    // DELETE /revenue/{id}  -> (método 'destroy')
-    // E também: show, create, edit
     Route::resource('revenue', RevenueController::class);
 
-    // --- FIM DAS ADIÇÕES ---
 });
 
 require __DIR__.'/settings.php';
